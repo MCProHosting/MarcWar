c2db56ea9ca11c96426021ba7de0d4a140f53aa2
Evan Lindsay <evanlindsay@live.com>
2013-10-19 23:24:12 -0700

    Set player inventory when they join.

--- src/com/mcprohosting/plugins/marcwar/entities/Participant.java
+++ src/com/mcprohosting/plugins/marcwar/entities/Participant.java
@@ -1,5 +1,10 @@
 package com.mcprohosting.plugins.marcwar.entities;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
 public class Participant {
 
 	private String name;
@@ -10,6 +15,8 @@ public class Participant {
 		this.name = name;
 		this.team = team;
 		this.hasFlag = false;
+
+		this.setInventory();
 	}
 
 	public String getName() {
@@ -26,6 +33,21 @@ public class Participant {
 
 	public void setFlag() {
 		hasFlag = !hasFlag;
+	}
+
+	public void setInventory() {
+		PlayerInventory inventory = Bukkit.getPlayer(this.getName()).getInventory();
+		inventory.clear();
+		inventory.addItem(new ItemStack(Material.IRON_AXE));
+		inventory.setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+		inventory.setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+		inventory.setBoots(new ItemStack(Material.IRON_BOOTS));
+
+		if (team.equalsIgnoreCase("blue")) {
+			inventory.setHelmet(new ItemStack(Material.WOOL, 1, (short) 11));
+		} else {
+			inventory.setHelmet(new ItemStack(Material.WOOL, 1, (short) 14));
+		}
 	}
 
 }
